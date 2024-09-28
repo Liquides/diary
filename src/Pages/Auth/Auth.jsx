12f1de9a508d0fd9838b97127179fe7d56a49676
Liquides <drilllci@gmail.com>
2024-09-28 15:14:07 +0300

    update

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -4,6 +4,9 @@ import NavBar from "../../Components/NavBar/NavBar";
 import { auth } from "../../Components/Functions/AuthDiary";
 import { useNavigate } from "react-router-dom";
 export const Auth = () => {
+  const login = useRef(null);
+  const password = useRef(null);
+  const navigate = useNavigate();
   return (
     <div className="wrapper">
       <NavBar />
@@ -26,6 +29,25 @@ export const Auth = () => {
               ref={password}
             />
           </div>
+        </div>
+        <div className="auth_button">
+          <button
+            onClick={async () => {
+              if (login.current.value && password.current.value) {
+                const isAuthenticated = await auth(
+                  login.current.value,
+                  password.current.value
+                );
+                if (isAuthenticated) {
+                  // navigate("/");
+                } else {
+                  alert("Неверный логин или пароль");
+                }
+              }
+            }}
+          >
+            Войти
+          </button>
         </div>
       </div>
     </div>
