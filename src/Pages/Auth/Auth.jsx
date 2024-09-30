33c5f0277e26fc5f2648e23dc09a680b9027054d
Liquides <drilllci@gmail.com>
2024-09-30 15:53:44 +0300

    update

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -1,20 +1,17 @@
-import React, { useRef } from 'react';
-import '../../assets/styles/main.scss';
-import TitleBar from '../../Components/TitleBar/TitleBar';
-import { auth } from '../../Components/Functions/AuthDiary';
-import { useNavigate } from 'react-router-dom';
+import React, { useRef } from "react";
+import "../../assets/styles/main.scss";
+import TitleBar from "../../Components/TitleBar/TitleBar";
+import { auth } from "../../Components/Functions/AuthDiary";
+import { useNavigate } from "react-router-dom";
 export const Auth = () => {
   const login = useRef(null);
   const password = useRef(null);
   const navigate = useNavigate();
   return (
     <div className="wrapper">
-      <TitleBar />
       <div className="auth">
         <div className="form">
-          <div className="title">
-            <h2>Авторизация</h2>
-          </div>
+          <div className="title">{/* <h2>Авторизация</h2> */}</div>
           <div className="inputs">
             <input
               type="text"
@@ -28,6 +25,10 @@ export const Auth = () => {
               autoComplete="off"
               ref={password}
             />
+            <div className="check">
+              <input type="checkbox" id="check" />
+              <label htmlFor="check">Запомнить меня</label>
+            </div>
           </div>
         </div>
         <div className="auth_button">
@@ -39,14 +40,14 @@ export const Auth = () => {
                   password.current.value
                 );
                 if (isAuthenticated) {
-                  // navigate("/");
+                  navigate("/");
                 } else {
-                  alert('Неверный логин или пароль');
+                  alert("Неверный логин или пароль");
                 }
               }
             }}
           >
-            Войти
+            Авторизация
           </button>
         </div>
       </div>
